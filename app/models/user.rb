f8d9a0fabc037a2cfa7e760b93d8d40990425a41
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-15 16:40:22 -0400

    fixed typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true, uniqueness: true 
-  validates :email, format: { withL URI::MailTo::EMAIL_REGEXP}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
   validates :password, length: { minimum: 6}
 
   has_many :services
   has_many :listings 
-  has_many :materials :through => :listings 
+  has_many :materials, through: :listings 
 
 end
