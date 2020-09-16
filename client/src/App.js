d464894492393483f7473a9ca365bcecc87c11bc
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 10:11:32 -0400

    adding footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch } from 'react-router-dom'
 import Header from './components/Header/Header.jsx'
+// import Layout from './layouts/Layout.jsx'
 import './App.css';
 
 
 function App() {
   return (
     // <h1></h1>
-    <Header/>
+    // <Layout/>
+    <Header />
   )
 }
 
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,2 +1,10 @@
 import React from 'react';
 import Header from './components/Header/Header.jsx'
+
+export default function Layout() {
+  return (
+  <>
+    <Header/>
+    </>
+  )
+}
