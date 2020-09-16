39e56f7fd84550d17f814202326f2e12b2dc733f
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 11:45:17 -0400

    completed layout

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../layouts/Layout.jsx'
+import './Home.css';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className='items'>
+      <img clasName='fishing' src="https://i.ibb.co/YjD6b2X/Fishing.jpg" alt="Fishing" border="0" />
+      </div>
+    </Layout>
+  )
+}
