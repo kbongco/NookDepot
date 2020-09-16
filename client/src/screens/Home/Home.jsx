8d02147cf26b136dc72283243ab8fd6202602f0e
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 12:04:57 -0400

    added pictures for front page

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -6,8 +6,17 @@ export default function Home() {
   return (
     <Layout>
       <div className='items'>
-      <img clasName='fishing' src="https://i.ibb.co/YjD6b2X/Fishing.jpg" alt="Fishing" border="0" />
+        <h5>Here are some things you can find here ranging from DIY materials, to gigs, to 
+        plants to tools!
+        </h5>
+        <div className='info-container'>
+        <img className='diy-mats' src="https://i.ibb.co/wMKW8qs/gold.png" alt="gold" border="0"/>
+          <img className='plants' src="https://i.ibb.co/Qdm7p0G/peach.png" alt="peach" border="0" />
+          <img className='tools' src="https://i.ibb.co/2P14fnx/fishingrod.png" alt="fishingrod" border="0"/>
+          <img className='fishing' src="https://i.ibb.co/YjD6b2X/Fishing.jpg" alt="Fishing" border="0" />
+        </div>
       </div>
+      <div className=
     </Layout>
   )
 }
