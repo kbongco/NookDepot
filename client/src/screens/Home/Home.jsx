2bb6cb3840186eca4afc6dee7f1b19fbe61762bc
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 12:41:05 -0400

    fixed footer, adding on-demand section

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout>
       <div className='items'>
-        <h5>Here are some things you can find here ranging from DIY materials, to gigs, to 
+        <h5 className='whatcanIfind'>Here are some things you can find here ranging from DIY materials, to gigs, to 
         plants to tools!
         </h5>
         <div className='info-container'>
@@ -16,15 +16,16 @@ export default function Home() {
           <img className='fishing' src="https://i.ibb.co/YjD6b2X/Fishing.jpg" alt="Fishing" border="0" />
         </div>
       </div>
-      <div className='current-demand'>
-        <div className='current-container'>
-          </div>
-
-
+      <div className='popular'>
+        <h4>Currently On Demand</h4>
+        <div className='on-demand'>
+          <img className='sakura' src="https://i.ibb.co/3z7K5x1/cherryblossom.png" alt="cherryblossom" border="0" />
+          <img className='rusted' src="https://i.ibb.co/yNH9bW6/blueroses.png" alt="blueroses" border="0"/>
+        </div>
       </div>
       <div className='about-nook'>
       <div className='detailed'>
-        <h2>What is NookDepot?</h2>
+        <h2 className='about-it'>What is NookDepot?</h2>
         <p>Inspired by a certain website
           where you can buy/sell home-made items as well as a certain website 
           for the animal crossing series. NookDepot focuses more on DIY Materials, DIY Recipes, Tools and plants as well as various gigs like bug-catching, and material gathering. 
