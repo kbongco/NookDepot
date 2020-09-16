c01c8a4f94a05dc9a92a5d09f6af8f600b28767a
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 10:27:33 -0400

    adding css for footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Footer.css';
 
 
 export default function Footer() {
@@ -8,10 +9,11 @@ export default function Footer() {
         <p>
           This project was a made for fun project. I am not affiliated with Nintendo, Etsy, Amazon, Nookazon, etc. I'm just a gamer who is also an aspiring software developer. Credits to the following sites for providing the datasets files for use.
         </p>
-        <a href="https://github.com/Norviah/animal-crossing">
+        <a id ='json' href="https://github.com/Norviah/animal-crossing">
           Animal Crossing JSON
         </a>
-        <a href="https://docs.google.com/spreadsheets/d/1i1nFE0GUCHQS0eWjPuUmYbdtEbeF0Pb-/edit#gid=163065426">
+        |
+        <a id ='google' href="https://docs.google.com/spreadsheets/d/1i1nFE0GUCHQS0eWjPuUmYbdtEbeF0Pb-/edit#gid=163065426">
           SpreadSheet
         </a>
 
