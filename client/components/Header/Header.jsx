5cd53388c48440f3cb11496adfa4b76e39db0147
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 00:05:05 -0400

    added components

---
+++ client/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="header-content">
+          <h1 className='title'>NookDepot</h1>
+          <ul className='navLinks'>
+            <li className='pageLinks'>
+              <NavLink exact to='/'>
+                Home 
+              </NavLink>
+            </li>
+            <li className='pageLinks'>
+              <NavLink exact to='/directory'>
+                Directory 
+              </NavLink>
+            </li>
+            <li className='pageLinks'>
+              <NavLink exact to='/Tools'>
+                Tools
+              </NavLink>
+            </li>
+          </ul>
+          </div>
+      </header>
+      </>
+  )
+}
