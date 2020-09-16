3f71f8098e93816315427a200460ee7dc4615217
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 10:18:29 -0400

    completed footer, need styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch } from 'react-router-dom'
 import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 // import Layout from './layouts/Layout.jsx'
 import './App.css';
 
@@ -9,7 +10,10 @@ function App() {
   return (
     // <h1></h1>
     // <Layout/>
+    <>
     <Header />
+    <Footer />
+      </>
   )
 }
 
--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,16 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer>
-    <div class='disclaimer'>
+      <div class='disclaimer'>
+        <p>
+          This project was a made for fun project. I am not affiliated with Nintendo, Etsy, Amazon, Nookazon, etc. I'm just a gamer who is also an aspiring software developer. Credits to the following sites for providing the datasets files for use.
+        </p>
+        <a href="https://github.com/Norviah/animal-crossing">
+          Animal Crossing JSON
+        </a>
+        <a href="https://docs.google.com/spreadsheets/d/1i1nFE0GUCHQS0eWjPuUmYbdtEbeF0Pb-/edit#gid=163065426">
+          SpreadSheet
+        </a>
 
       </div>
       </footer>
