35f60dd81fe7f43db1626671c1d8a54807f1bf42
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-17 11:57:29 -0400

    fixed login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     if @user.save
       @token = encode({id: @user.id})
       render json: {
-        user: @user.attributes.except('password_digest'),
+        user: @user.attributes.except('password'),
         token: @token
         }, status: :created
     else
