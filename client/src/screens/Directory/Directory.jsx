e33e7bea745eb7d0eb9b9aee670c23884e388c03
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-16 22:14:47 -0400

    completed some directory files, will return later

--- client/src/screens/Directory/Directory.jsx
+++ client/src/screens/Directory/Directory.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../../layouts/Layout.jsx'
+import './Directory.css'
 
 export default function Directory(props) {
   return (
     <Layout>
-      oh hai
+      <h1 className='current-listings'>Check out the current Listings here!</h1>
+      <h4>Want to add your listing? Go Here</h4>
     </Layout>
   )
 }
