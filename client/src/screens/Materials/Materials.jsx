7a50b9cdc05505952294486ae90b87225d5317fb
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-17 10:16:24 -0400

    added flexbox for css

--- client/src/screens/Materials/Materials.jsx
+++ client/src/screens/Materials/Materials.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Layout from '../../layouts/Layout.jsx'
 
+import './Materials.css'
+
 export default function Materials(props) {
   const {materials} = props
   console.log(props)
 
   return (
     <Layout>
-      <h1>DIY Materials</h1>
+      <div className='materialparent-container'>
+      <h1 className='DIYmats'>DIY Materials</h1><br/>
       {materials.map((material) => (
          <div container='material-container'>
-         <p>{material.name}</p>
+         <p className='material-name'>{material.name}</p>
           <img className='material-icon' src={material.imgURL} />
-          </div>
-       ))} 
+      </div>
+      ))} 
+      </div>
     </Layout>
   )
 }
