4798a6d84d794318fb916d1ffd895c834c4dec3f
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-17 00:32:49 -0400

    finished adding underconstruction component

--- client/src/containers/MainContainer.jsx
+++ client/src/containers/MainContainer.jsx
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom'
 import Home from "../screens/Home/Home.jsx";
 import Directory from '../screens/Directory/Directory.jsx'
+import UnderConstruction from "../screens/UnderConstruction/UnderConstruction.jsx";
 
 
 export default function MainContainer(props) {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/directory' component={Directory}>
-
-      </Route>
+      <Route path='/directory' component={Directory}/>
+      <Route path='/tools' component={UnderConstruction} />
+      <Route path='/garden' component={UnderConstruction} />
+      <Route path='/DIY-Recipes' component={UnderConstruction} />
     </Switch>
   )
 }
