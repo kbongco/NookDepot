913c83a15f3c8010b84a1cd9656ca065d365ae20
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-19 16:51:06 -0400

    fixing materials details

--- client/src/screens/MaterialsDetail/MaterialsDetail.jsx
+++ client/src/screens/MaterialsDetail/MaterialsDetail.jsx
@@ -3,30 +3,33 @@ import { useParams } from 'react-router-dom'
 import { getOneMaterial } from '../../services/materials.js'
 
 export default function MaterialsDetail(props) { 
-  const [material, updateMaterial] = useState(null);
-  const [materialId, updateMaterialId] = useState('');
+
+  const [material, updateMaterials] = useState(null);
+  const [materialId, updateMaterialId] = useState('')
   const { id } = useParams();
   const { materials } = props
+
   console.log(props)
 
   useEffect(() => {
     const fetchMaterial = async () => {
-      const oneMaterial = await getOneMaterial(id)
-      updateMaterial(oneMaterial)
+      const singleMaterial = await getOneMaterial(id)
+      updateMaterials(singleMaterial)
     }
     fetchMaterial();
   }, [])
 
+  const material = materials.find(m => m.id === id )
   const onematerial = props.material.filter (material => material.material_id)
 
 
   return (
     <div>
       <img className='materials-img'
-        src={materials.imgURL} />
-      <h1>{materials.name}</h1>
-      <p>{materials.notes}</p>
-      <p>{materials.season}</p>
+        src={material.imgURL} />
+      <h1>{material.name}</h1>
+      <p>{material.notes}</p>
+      <p>{material.season}</p>
 
     </div>
   )
--- client/src/services/towninfo.js
+++ client/src/services/towninfo.js
@@ -3,7 +3,7 @@ import api from './api-config'
 
 
 export const postTownInfo = async (user_id,formData) => {
-  const resp = await api.post(`/users/${user_id}/towninfos`, {towninfos:formData})
+  const resp = await api.post(`/users/${user_id}/towninfos`, {towninfo:formData})
   return resp.data 
 }
 
@@ -14,6 +14,6 @@ export const getOneTownInfo = async (user_id) => {
 }
 
 export const putTownInfo = async (id, formData) => {
-  const resp = await api.put(`/users/${id}/towninfos/${id}`,{towninfos:formData})
+  const resp = await api.put(`/users/${id}/towninfos/${id}`,{towninfo:formData})
   return resp.data 
 }
