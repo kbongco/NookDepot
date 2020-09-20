c13e50aa909d58a546ab27499ce763df526d1ff0
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-19 22:44:10 -0400

    added ad fixed css

--- client/src/screens/MaterialsDetail/MaterialsDetail.jsx
+++ client/src/screens/MaterialsDetail/MaterialsDetail.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getOneMaterial } from "../../services/materials.js";
 
+import './MaterialsDetail.css'
+
 export default function MaterialsDetail(props) {
   const [material, updateMaterials] = useState("");
   const { id } = useParams();
@@ -18,11 +20,14 @@ export default function MaterialsDetail(props) {
   }, []);
 
   return (
-    <div>
-      <img className="materials-img" src={material.imgURL} />
+    <div className='materials-detail'>
+      <img className="materials-detail-img" src={material.imgURL} />
+      <div className='materials'>
       <h1>{material.name}</h1>
       <p>{material.notes}</p>
-      <p>{material.season}</p>
+        <p>{material.season}</p>
+        <button>Add to my listing</button>
+      </div>
     </div>
   );
 }
