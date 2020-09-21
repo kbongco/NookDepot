8aa45a2798313a06ae6535fd4b1d2c513edd5ce2
Kathleen B <kathleenb@Kathleens-MacBook-Air.local>
2020-09-21 18:00:13 -0400

    fixed routing issues;

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   # get '/users/:id/towninfos' to 
+  post'/towninfos/:id', to: 'towninfos#create'
   get '/towninfos', to: 'towninfos#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: :create 
